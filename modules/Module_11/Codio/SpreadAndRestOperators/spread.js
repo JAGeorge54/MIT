4586793577cfa9f068d2a3d7a5fe6853c2a393af
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-02-08 13:03:34 -0500

    removed

--- modules/Module_11/Codio/SpreadAndRestOperators/spread.js
+++
@@ -1,6 +0,0 @@
-const terrestrials = ["fox", "lion", "elephant"];
-const aquatics = ["shark", "whale", "octopus"];
-
-// TODO: create the array allAnimals that contains all animals from terrestrials and aquatics arrays
-allAnimals = [...terrestrials, ...aquatics];
-console.log(allAnimals);
