f92a009220e75df1576139cf75aed55a013eb36f
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-02-08 10:19:29 -0500

    added module 3

---
+++ modules/module_3/officeHours/officeHours.js
@@ -0,0 +1,8 @@
+var number = 12;
+console.log('this is from java script file: ' + number);
+var counter = 0;
+while(counter < 10){
+    document.write('counter = ' + counter);
+    document.write("<br>");
+    counter++;
+}
