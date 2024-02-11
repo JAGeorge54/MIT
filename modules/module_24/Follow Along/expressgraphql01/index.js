7d27bb3ee12e3f94564ed2a4f1438141f519e8bc
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-02-11 11:57:04 -0500

    created graphql hello world

---
+++ modules/module_24/Follow Along/expressgraphql01/index.js
@@ -0,0 +1,31 @@
+var express = require("express")
+var { graphqlHTTP } = require("express-graphql")
+var { buildSchema } = require("graphql")
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world!"
+  },
+}
+
+var app = express()
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+)
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Running Graphql on Port:${port}`)
+});
