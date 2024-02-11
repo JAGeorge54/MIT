36b6449ac3e12a3511df8ad556518ff4cb1cbad1
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-02-11 17:44:44 -0500

    added finle code

--- modules/module_24/Follow Along/graphqlRestaurants/index.js
+++ modules/module_24/Follow Along/graphqlRestaurants/index.js
@@ -70,13 +70,46 @@ var schema = buildSchema(`
   type Dish {
     name: String
     price: Int
+  },
+  input restaurantInput{
+  name: String
+  description: String
+  }
+  type DeleteResponse{
+    ok: Boolean!
+  }
+  type Mutation{
+    setrestaurant(input: restaurantInput): restaurant
+    deleterestaurant(id: Int!): DeleteResponse
+    editrestaurant(id: Int!, name: String!): restaurant
   }
 `)
 
 // The root provides a resolver function for each API endpoint
 var root = {
   restaurant : (arg)=> restaurants[arg.id],
-  restaurants : ()=> restaurants
+  restaurants : ()=> restaurants,
+  setrestaurant: ({ input }) => {
+    restaurants.push({ name: input.name, email: input.email, age: input.age });
+    return input;
+  },
+  deleterestaurant: ({ id }) => {
+    const ok = Boolean(restaurants[id]);
+    let delc = restaurants[id];
+    restaurants = restaurants.filter((item) => item.id !== id);
+    console.log(JSON.stringify(delc));
+    return { ok };
+  },
+  editrestaurant: ({ id, ...restaurant }) => {
+    if (!restaurants[id]) {
+      throw new Error("restaurant doesn't exist");
+    }
+    restaurants[id] = {
+      ...restaurants[id],
+      ...restaurant,
+    };
+    return restaurants[id];
+  },
 };
 
 var app = express()
