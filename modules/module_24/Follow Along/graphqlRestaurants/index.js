30963ce52a058e81ef0ba31bcd73652faf8ffd29
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-02-11 14:40:34 -0500

    added restaurants query

--- modules/module_24/Follow Along/graphqlRestaurants/index.js
+++ modules/module_24/Follow Along/graphqlRestaurants/index.js
@@ -3,18 +3,79 @@ var { graphqlHTTP } = require("express-graphql")
 var { buildSchema } = require("graphql")
 
 // Construct a schema, using GraphQL schema language
+
+    const restaurants = [
+      {
+        "name": "WoodsHill ",
+        "description": "American cuisine, farm to table, with fresh produce every day",
+        "dishes": [
+          {
+            "name": "Swordfish grill",
+            "price": 27
+          },
+          {
+            "name": "Roasted Broccily ",
+            "price": 11
+          }
+        ]
+      },
+      {
+        "name": "Fiorellas",
+        "description": "Italian-American home cooked food with fresh pasta and sauces",
+        "dishes": [
+          {
+            "name": "Flatbread",
+            "price": 14
+          },
+          {
+            "name": "Carbonara",
+            "price": 18
+          },
+          {
+            "name": "Spaghetti",
+            "price": 19
+          }
+        ]
+      },
+      {
+        "name": "Karma",
+        "description": "Malaysian-Chinese-Japanese fusion, with great bar and bartenders",
+        "dishes": [
+          {
+            "name": "Dragon Roll",
+            "price": 12
+          },
+          {
+            "name": "Pancake roll ",
+            "price": 11
+          },
+          {
+            "name": "Cod cakes",
+            "price": 13
+          }
+        ]
+      }
+    ]
+
 var schema = buildSchema(`
   type Query {
-    hello: String
+    restaurants: [Restaurant]
+  },
+  type Restaurant {
+    name: String
+    description: String
+    dishes: [Dish]
+  },
+  type Dish {
+    name: String
+    price: Int
   }
 `)
 
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return "Hello world!"
-  },
-}
+  restaurants : ()=> restaurants
+};
 
 var app = express()
 app.use(
