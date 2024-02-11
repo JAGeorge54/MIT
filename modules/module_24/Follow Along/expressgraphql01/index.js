66a6c32c2535b3eae1cdc4c54d0e2f8ebc369a29
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-02-11 11:59:17 -0500

    added contacts

--- modules/module_24/Follow Along/expressgraphql01/index.js
+++ modules/module_24/Follow Along/expressgraphql01/index.js
@@ -2,6 +2,39 @@ var express = require("express")
 var { graphqlHTTP } = require("express-graphql")
 var { buildSchema } = require("graphql")
 
+var contacts = [
+  {
+    id:1,
+    name: "peter parker",
+    age: 21,
+    email: "peter@mit.edu",
+    courses: [
+      { number: "1.00", name: "engr comp" },
+      { number: "3.00", name: "intro bio" }
+    ]
+  },
+  {
+    id:2,
+    name: "bruce wayne",
+    age: 32,
+    email: "bruce@mit.edu",
+    courses: [
+      { number: "2.00", name: "intro ME" },
+      { number: "3.00", name: "intro MS" }
+    ]
+  },
+  {
+    id:3,
+    name: "diana prince",
+    age: 25,
+    email: "diana@mit.edu",
+    courses: [
+      { number: "2.00", name: "intro arch" },
+      { number: "1.00", name: "intro chem" }
+    ]
+  },
+];
+
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
