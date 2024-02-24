12731c3b757abdeecede52ed5bbcbb819b323d0a
JAGeorge54 <jesse.george54@gmail.com>
2024-02-24 11:58:57 -0500

    added code

--- modules/module_25/Follow Along/books/app.js
+++ modules/module_25/Follow Along/books/app.js
@@ -3,6 +3,39 @@ const app     = express();
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUI = require('swagger-ui-express');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Library API',
+            version: '1.0.0'
+        }
+    },
+    apis: ['app.js']
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /books:
+ *  get:
+ *      description: get all books
+ *      responses:
+ *          200:
+ *              description: Success    
+ */
+
+app.get('/books', (req, res) => {
+    res.send([
+        {
+            isbn : '9781781100486',
+            title: 'Harry Potter and the Sorcerer\'s Stone',
+            author: 'J.K. Rowling',
+            publisher: 'Scholastic'
+        }
+    ]);
+});
+
+app.listen(3000, () => {
+    console.log('Running on port 3000');
 })
