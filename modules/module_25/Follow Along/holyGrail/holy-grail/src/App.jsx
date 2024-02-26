0d677e79189610d9ba0844ce5d52d94451123f8c
JAGeorge54 <jesse.george54@gmail.com>
2024-02-26 10:10:13 -0500

    imported components

--- modules/module_25/Follow Along/holyGrail/holy-grail/src/App.jsx
+++ modules/module_25/Follow Along/holyGrail/holy-grail/src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 import './App.css'
+import article from './components/article'
+import footer from './components/footer'
+import header from './components/header'
+import left from './components/left'
+import right from './components/right'
 
 function App() {
 
