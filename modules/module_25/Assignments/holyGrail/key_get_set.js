99a424176ee21042a4dca608a6bb5a516c9b546e
JAGeorge54 <jesse.george54@gmail.com>
2024-02-27 15:02:03 -0500

    fixed typo

--- modules/module_25/Assignments/holyGrail/key_get_set.js
+++ modules/module_25/Assignments/holyGrail/key_get_set.js
@@ -6,8 +6,8 @@ client.get('my_key', function(err, reply) {
     console.log(reply);
 })
 
-client.mset('header', 0,'left', 0,'article', 0,'right', 0,'footer', 0)
-client.mget(['header', 'left', 'article', 'right', 'footer'], function(err, value){
+client.set('header', 0,'left', 0,'article', 0,'right', 0,'footer', 0)
+client.get(['header', 'left', 'article', 'right', 'footer'], function(err, value){
     console.log(value);
 })
 
