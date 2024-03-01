d2cc154db103848db5d82d4ae4d022fd0b8a9850
JAGeorge54 <jesse.george54@gmail.com>
2024-03-01 09:11:31 -0500

    added database js file

---
+++ modules/module_26/Follow Along/firebase/database.js
@@ -0,0 +1,20 @@
+(function() {
+    // Your web app's Firebase configuration
+	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: "AIzaSyBDE4gVWQeNfsguvIy3Qdyr5GnwdMInZnI",
+        authDomain: "courso-10521.firebaseapp.com",
+        projectId: "courso-10521",
+        storageBucket: "courso-10521.appspot.com",
+        messagingSenderId: "709929043728",
+        appId: "1:709929043728:web:cfd5395358a229d9580795",
+        measurementId: "G-BK1F6NGQY9"
+      };
+
+    // Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
+
+
+	// handle on firebase db
+	const db = firebase.database();
+});
