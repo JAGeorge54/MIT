ac74500bcf06816e4fff20bc3663bf3b0fa79e24
JAGeorge54 <jesse.george54@gmail.com>
2024-03-08 10:17:42 -0500

    changed navbar brand

--- modules/module_27/Follow Along/badBank/modules.js
+++ modules/module_27/Follow Along/badBank/modules.js
@@ -2,7 +2,7 @@ const ui = {};
 
 ui.navigation = `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#" onclick="defaultModule()">Navbar</a>
+        <a class="navbar-brand" href="#" onclick="defaultModule()">BadBank</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
