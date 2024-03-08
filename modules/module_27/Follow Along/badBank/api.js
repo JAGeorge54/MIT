786e03c50116d9d7df1584e4ed7cb015b24cb9aa
JAGeorge54 <jesse.george54@gmail.com>
2024-03-08 13:50:12 -0500

    added login function

--- modules/module_27/Follow Along/badBank/api.js
+++ modules/module_27/Follow Along/badBank/api.js
@@ -34,3 +34,27 @@ function allData() {
     const status = document.getElementById('allDataStatus');
     status.innerHTML = JSON.stringify(ctx.accounts);
 };
+
+function login() {
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const status = document.getElementById('loginStatus');
+    let user = {};
+
+    function isUser(account) {
+        return account.email === email.value;
+    }
+
+    function findUser() {
+        let tempUser = ctx.accounts.find(isUser)
+        if(password.value === tempUser.password){
+            ctx.loggedIn = tempUser;
+            email.value = '';
+            password.value = '';
+            status.innerHTML = 'Login Successful!'
+            
+        } else status.innerHTML = "Incorrect username or password";
+    }
+
+    findUser();
+};
