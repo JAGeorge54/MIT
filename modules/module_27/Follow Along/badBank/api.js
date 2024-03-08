61d4c2b110095c4b3b515614d4a957722320d847
JAGeorge54 <jesse.george54@gmail.com>
2024-03-08 11:05:20 -0500

    created context

---
+++ modules/module_27/Follow Along/badBank/api.js
@@ -0,0 +1,12 @@
+// context is globally shared
+const ctx = {
+    "accounts": [
+        {
+            "name": "peter",
+            "email": "peter@mit.edu",
+            "balance": 0,
+            "password": "secret"
+        }
+    ]
+};
+
