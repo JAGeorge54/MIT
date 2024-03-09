999c67493a2288c46a02f800941beb76efb5a2ae
JAGeorge54 <jesse.george54@gmail.com>
2024-03-09 12:49:48 -0500

    added remaining components and routes

--- modules/module_27/Follow Along/badBankReact/badBank/src/App.jsx
+++ modules/module_27/Follow Along/badBankReact/badBank/src/App.jsx
@@ -7,6 +7,11 @@ import Home from './components/home'
 import AllData from './components/alldata'
 import CreateAccount from './components/createaccount'
 import NavBar from './components/navbar'
+import Login from './components/login'
+import Balance from './components/balance'
+import Deposit from './components/deposit'
+import Withdraw from './components/withdraw'
+import Logout from './components/logout'
 
 function App() {
   
@@ -17,6 +22,12 @@ function App() {
       <Routes>
         <Route path='/' element={ <Home/> } />
         <Route path='/createaccount' element={ <CreateAccount/> } />
+        <Route path='/login' element={ <Login/> } />
+        <Route path='/balance' element={ <Balance/> } />
+        <Route path='/deposit' element={ <Deposit/> } />
+        <Route path='/withdraw' element={ <Withdraw/> } />
+        <Route path='/logout' element={ <Logout/> } />
+        <Route path='/alldata' element={ <AllData/> } />
       </Routes>
     </>
   )
