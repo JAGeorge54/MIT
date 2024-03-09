36043ef69ebd87e7085d29895661acf6b7905312
JAGeorge54 <jesse.george54@gmail.com>
2024-03-09 10:58:22 -0500

    added components for test

--- modules/module_27/Follow Along/badBankReact/badBank/src/App.jsx
+++ modules/module_27/Follow Along/badBankReact/badBank/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import { ContextProvider } from './context'
 import Home from './components/home'
+import AllData from './components/alldata'
+import CreateAccount from './components/createaccount'
+import NavBar from './components/navbar'
 
 function App() {
   
 
   return (
     <>
+      <NavBar/>
       <Home/>
+      <AllData/>
+      <CreateAccount/>
     </>
   )
 }
