85ddc6ad412a5324adc847bf62058b9d3d577998
JAGeorge54 <jesse.george54@gmail.com>
2024-03-09 13:17:35 -0500

    added spacing to imported components

--- modules/module_27/Follow Along/badBankReact/badBank/src/App.jsx
+++ modules/module_27/Follow Along/badBankReact/badBank/src/App.jsx
@@ -3,15 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import { ContextProvider } from './context'
-import Home from './components/home'
-import AllData from './components/alldata'
+import Home          from './components/home'
+import AllData       from './components/alldata'
 import CreateAccount from './components/createaccount'
-import NavBar from './components/navbar'
-import Login from './components/login'
-import Balance from './components/balance'
-import Deposit from './components/deposit'
-import Withdraw from './components/withdraw'
-import Logout from './components/logout'
+import NavBar        from './components/navbar'
+import Login         from './components/login'
+import Balance       from './components/balance'
+import Deposit       from './components/deposit'
+import Withdraw      from './components/withdraw'
+import Logout        from './components/logout'
 
 function App() {
   
