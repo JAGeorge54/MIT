a6487b41b8d13a249fdf0703ba6cc6bab8e83b5f
JAGeorge54 <jesse.george54@gmail.com>
2024-03-10 11:08:14 -0400

    displayed data with json

--- modules/module_27/Follow Along/badBankReact/badBank/src/components/alldata.jsx
+++ modules/module_27/Follow Along/badBankReact/badBank/src/components/alldata.jsx
@@ -1,6 +1,24 @@
+import { useState, useContext } from "react";
+import UserContext from '../context/UserContext';
+
 function AllData() {
+    const {user, setUser} = useContext(UserContext);
+
+    function Users () {
+        let users = user[0].accounts.map((account, i) => {
+            return <p>{JSON.stringify(account)}</p>
+        })
+        return (
+            <p>{users}</p>
+        )
+    }
+        
+
     return(
-        <h1>alldata</h1>
+        <>
+            <h1>alldata</h1>
+            <Users/>
+        </>
     )
 }
 
