fb244c83abf7f2fdb763b7fc86fde367be8456c7
JAGeorge54 <jesse.george54@gmail.com>
2024-03-10 13:17:02 -0400

    added login card

--- modules/module_27/Follow Along/badBankReact/badBank/src/components/createAccount/createaccount.jsx
+++ modules/module_27/Follow Along/badBankReact/badBank/src/components/createAccount/createaccount.jsx
@@ -1,25 +1,27 @@
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import './createAccount.css'
 
 function CreateAccount() {
-    const CreateAccount = () => {
-        return (
-            <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                    </Card.Text>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-            </Card>
-        )
-    }
+    
 
     return(
-        <CreateAccount/>
+        <div id='card' className="d-flex justify-content-center col text-center">
+                <Card bg='secondary' text='white' border='dark' style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Create Account</Card.Title>
+                        <Card.Text>
+                            <p>Name</p>
+                            <input type='text' id='name' placeholder='Enter a Name'/><br/><br/>
+                            <p>Email</p>
+                            <input type="text" id="email" placeholder='Enter an Email Address'/><br/><br/>
+                            <p>Password</p>
+                            <input type="text" id="email" placeholder='Enter a Password'/><br/><br/>
+                            <Button variant="primary">Submit</Button>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
     )
 }
 
