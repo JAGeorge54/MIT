d6bb98d6c635c7bc696815a40f6d8ce0f8d7c8db
JAGeorge54 <jesse.george54@gmail.com>
2024-03-10 11:07:51 -0400

    added objects to context

--- modules/module_27/Follow Along/badBankReact/badBank/src/context/UserContextProvider.jsx
+++ modules/module_27/Follow Along/badBankReact/badBank/src/context/UserContextProvider.jsx
@@ -3,7 +3,16 @@ import React, { useState } from 'react'
 import UserContext from './UserContext'
 
 const UserContextProvider = ({children}) => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState([{accounts: [
+        {
+            'name': 'peter',
+            'email': 'peter@mit.edu',
+            'password': 'secret',
+            'balance': 100
+        }
+    ]}, 
+        {'loggedin': false}
+    ]);
     return (
         <UserContext.Provider value={{user, setUser}}>
             {children}
