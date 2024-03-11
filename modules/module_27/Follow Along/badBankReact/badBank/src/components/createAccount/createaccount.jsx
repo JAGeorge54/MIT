a175464cd72113d9c90a9b99b352c028d56ad2ea
JAGeorge54 <jesse.george54@gmail.com>
2024-03-11 09:45:01 -0400

    added onchange to save data to context

--- modules/module_27/Follow Along/badBankReact/badBank/src/components/createAccount/createaccount.jsx
+++ modules/module_27/Follow Along/badBankReact/badBank/src/components/createAccount/createaccount.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import UserContext from '../../context/UserContext'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import './createAccount.css'
 
 function CreateAccount() {
+    const {users, setUsers} = useContext(UserContext);
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        setUsers([...users, {name, email, password, "balance": 100}])
+        let nameReset = document.getElementById('name');
+        nameReset = '';
+    }
 
     return(
         <div id='card' className="d-flex justify-content-center col text-center">
@@ -15,12 +23,30 @@ function CreateAccount() {
                         <Card.Title>Create Account</Card.Title>
                         <Card.Text>
                             <p>Name</p>
-                            <input type='text' id='name' placeholder='Enter a Name'/><br/><br/>
+                            <input 
+                                type='text' 
+                                id='name' 
+                                placeholder='Enter a Name' 
+                                onChange={e => {
+                                    setName(e.currentTarget.value)
+                                }}/><br/><br/>
                             <p>Email</p>
-                            <input type="text" id="email" placeholder='Enter an Email Address'/><br/><br/>
+                            <input 
+                                type="text" 
+                                id="email" 
+                                placeholder='Enter an Email Address'
+                                onChange={e => {
+                                    setEmail(e.currentTarget.value)
+                                }}/><br/><br/>
                             <p>Password</p>
-                            <input type="text" id="email" placeholder='Enter a Password'/><br/><br/>
-                            <Button variant="primary">Submit</Button>
+                            <input 
+                                type="text" 
+                                id="password" 
+                                placeholder='Enter a Password'
+                                onChange={e => {
+                                    setPassword(e.currentTarget.value)
+                                }}/><br/><br/>
+                            <Button variant="primary" onClick={e => handleSubmit(e)}>Submit</Button>
                         </Card.Text>
                     </Card.Body>
                 </Card>
