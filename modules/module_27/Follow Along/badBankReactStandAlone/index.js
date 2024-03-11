765f7672d25d636bb5cbbd78620650de9479f70f
JAGeorge54 <jesse.george54@gmail.com>
2024-03-11 12:26:15 -0400

    added context wrapper

--- modules/module_27/Follow Along/badBankReactStandAlone/index.js
+++ modules/module_27/Follow Along/badBankReactStandAlone/index.js
@@ -2,12 +2,14 @@ function Spa () {
     return (
     <HashRouter>
       <div>
-        <NavBar/>        
-        <div className="container" style={{padding: "20px"}}>
+        <NavBar/>
+        <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
+          <div className="container" style={{padding: "20px"}}>
             <Route path="/" exact component={Home} />
             <Route path="/CreateAccount/" component={CreateAccount} />
             <Route path="/alldata/" component={AllData} />
           </div>
+        </UserContext.Provider>
       </div>
     </HashRouter>
     )
