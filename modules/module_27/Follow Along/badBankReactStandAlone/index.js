b90fec5838ccaef269fe477c8e6b51796c4737f6
JAGeorge54 <jesse.george54@gmail.com>
2024-03-11 12:11:19 -0400

    added hashroute wrapper and home, createaccount, and alldata components

--- modules/module_27/Follow Along/badBankReactStandAlone/index.js
+++ modules/module_27/Follow Along/badBankReactStandAlone/index.js
@@ -1,6 +1,15 @@
 function Spa () {
     return (
-        <NavBar/>
+    <HashRouter>
+      <div>
+        <NavBar/>        
+        <div className="container" style={{padding: "20px"}}>
+            <Route path="/" exact component={Home} />
+            <Route path="/CreateAccount/" component={CreateAccount} />
+            <Route path="/alldata/" component={AllData} />
+          </div>
+      </div>
+    </HashRouter>
     )
 }
 
