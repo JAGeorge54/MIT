458b66c0021a6b69cb400d92c0783f9617949561
JAGeorge54 <jesse.george54@gmail.com>
2024-03-11 14:51:32 -0400

    added code for functions and form

--- modules/module_27/Follow Along/badBankReactStandAlone/createaccount.js
+++ modules/module_27/Follow Along/badBankReactStandAlone/createaccount.js
@@ -1,10 +1,64 @@
 function CreateAccount() {
+    const [status, setStatus] = React.useState('');
+    const [show, setShow]     = React.useState(true);
+
     return (
         <Card 
             bgcolor='primary'
             header='Create Account'
-            status=''
-            body=''
+            status={status}
+            body={show ? 
+                <CreateForm setShow={setShow} /> :
+                <CreateMsg setShow={setShow} />}
         />
+    );
+}
+
+function CreateMsg(props) {
+    return (
+        <>
+            <h5>Success!</h5>
+            <button type='submit'
+            className='btn btn-light'
+            onClick={() => props.setShow(true)}>Add another account</button>
+        </>
+    );
+}
+
+function CreateForm(props) {
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const ctx = React.useContext(UserContext);
+
+    function handle() {
+        console.log(name, email, password);
+        ctx.users.push({name, email, password, 'balance': 100});
+        props.setShow(false);
+    }
+    return (
+        <>
+            Name<br/>
+            <input type='input'
+            className='form-control'
+            placeholder='Enter name'
+            value={name}
+            onChange={e => setName(e.currentTarget.value)} /><br/>
+            Email<br/>
+            <input type='input'
+            className='form-control'
+            placeholder='Enter email'
+            value={email}
+            onChange={e => setEmail(e.currentTarget.value)} /><br/>
+            Password<br/>
+            <input type='input'
+            className='form-control'
+            placeholder='Enter password'
+            value={password}
+            onChange={e => setPassword(e.currentTarget.value)} /><br/>
+            <button type='submit'
+            className='btn btn-light'
+            onClick={handle}>Create Account</button>
+        </>
     );
 }
