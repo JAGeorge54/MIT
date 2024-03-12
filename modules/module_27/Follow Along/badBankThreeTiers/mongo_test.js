e9b21348f57f32e04648e0ca9ef8ab83c965b7b1
JAGeorge54 <jesse.george54@gmail.com>
2024-03-12 11:24:06 -0400

    insert into customer table

--- modules/module_27/Follow Along/badBankThreeTiers/mongo_test.js
+++ modules/module_27/Follow Along/badBankThreeTiers/mongo_test.js
@@ -11,4 +11,11 @@ MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
     // new user
     var name = 'user' + Math.floor(Math.random()*10000);
     var email = name + '@mit.edu';
+
+    //insert into customer table
+    var collection = db.collection('customers');
+    var doc = {name, email};
+    collection.insertOne(doc, {w:1}, function(err, result) {
+        console.log('Document insert')
+    })
 });
