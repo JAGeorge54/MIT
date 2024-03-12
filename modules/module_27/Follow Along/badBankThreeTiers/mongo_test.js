05dc396d488caf19efb8b08a4e3fa8cbbf1ed090
JAGeorge54 <jesse.george54@gmail.com>
2024-03-12 10:21:05 -0400

    connected mongo to docker image, added colsolelog

--- modules/module_27/Follow Along/badBankThreeTiers/mongo_test.js
+++ modules/module_27/Follow Along/badBankThreeTiers/mongo_test.js
@@ -1,2 +1,6 @@
 const MongoClient = require('mongo').MongoClient;
 const url = 'mongodb://localhost:27017';
+
+MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+    console.log('Connected!')
+});
