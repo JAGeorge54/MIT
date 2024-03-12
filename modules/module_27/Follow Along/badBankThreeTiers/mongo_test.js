958e9107a00403465e2237851f4964f68d83b2a4
JAGeorge54 <jesse.george54@gmail.com>
2024-03-12 11:20:26 -0400

    added database name

--- modules/module_27/Follow Along/badBankThreeTiers/mongo_test.js
+++ modules/module_27/Follow Along/badBankThreeTiers/mongo_test.js
@@ -2,5 +2,9 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 
 MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
-    console.log('Connected!')
+    console.log('Connected!');
+
+    //database name
+    const dbName = 'myproject';
+    const db = client.db(dbName);
 });
