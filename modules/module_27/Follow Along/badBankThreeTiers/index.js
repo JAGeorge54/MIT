743ce066e174301480cdf64e5a2a3515a5a6b3b0
JAGeorge54 <jesse.george54@gmail.com>
2024-03-12 08:38:22 -0400

    added express routes and listener

--- modules/module_27/Follow Along/badBankThreeTiers/index.js
+++ modules/module_27/Follow Along/badBankThreeTiers/index.js
@@ -5,3 +5,33 @@ var cors    =require('cors');
 //used to serve static files from public directory
 app.use(express.static('public'));
 app.use(cors());
+
+//create user account
+app.get('/account/create/:name/:email/:password', function (req, res) {
+    res.send({
+        name:     req.params.name,
+        email:    req.params.email,
+        password: req.params.password
+    });
+});
+
+//login user
+app.get('/account/login/:email/:password', function (req, res) {
+    res.send({
+        email:    req.params.email,
+        password: reqparams.password
+    });
+});
+
+//all acounts
+app.get('/account/all', function (req, res) {
+    res.send({
+        name:     'peter',
+        email:    'peter@mit.edu',
+        password: 'secret'
+    });
+});
+
+var port = 3000;
+app.listen(port);
+console.log('Running on port: ' + port);
