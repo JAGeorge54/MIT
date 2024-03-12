9e5e416d3733c49a4ee59b9aee7c1107927693ad
JAGeorge54 <jesse.george54@gmail.com>
2024-03-12 13:00:31 -0400

    practiced with mongo

--- modules/module_27/Follow Along/badBankThreeTiers/mongo_test.js
+++ modules/module_27/Follow Along/badBankThreeTiers/mongo_test.js
@@ -1,21 +1,67 @@
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
 const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
 
-MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
-    console.log('Connected!');
+// Database Name
+const dbName = 'myProject';
 
-    //database name
-    const dbName = 'myproject';
-    const db = client.db(dbName);
-
-    // new user
-    var name = 'user' + Math.floor(Math.random()*10000);
-    var email = name + '@mit.edu';
-
-    //insert into customer table
-    var collection = db.collection('customers');
-    var doc = {name, email};
-    collection.insertOne(doc, {w:1}, function(err, result) {
-        console.log('Document insert')
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  // new user
+  var name = 'user' + Math.floor(Math.random()*10000);
+  var email = name + '@mit.edu';
+  //insert into customer table
+  var collection = db.collection('customers');
+  var doc = {name, email};
+  collection.insertOne(doc, {w:1}, function(err, result) {
+      console.log('Document insert')
     })
-});
+
+    var customers = db
+        .collection('customers')
+        .find()
+        .toArray(function(err, docs){
+            console.log('collection:',docs)
+
+            client.close();
+        })
+
+
+  // the following code examples can be pasted here...
+
+//   return 'done.';
+}
+
+main()
+//   .then(console.log)
+//   .catch(console.error)
+//   .finally(() => client.close());
+
+// const MongoClient = require('mongodb').MongoClient;
+// const url = 'mongodb://localhost:27017';
+
+// MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+//     console.log('Connected!')
+
+//     //database name
+//     const dbName = 'myproject';
+//     const db = client.db(dbName);
+
+//     // new user
+//     var name = 'user' + Math.floor(Math.random()*10000);
+//     var email = name + '@mit.edu';
+
+//     //insert into customer table
+//     var collection = db.collection('customers');
+//     var doc = {name, email};
+//     collection.insertOne(doc, {w:1}, function(err, result) {
+//         console.log('Document insert')
+//     })
+// });
