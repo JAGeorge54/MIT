4ae6c6f40d9b483891e3e74327f75ff944a66780
JAGeorge54 <jesse.george54@gmail.com>
2024-03-15 13:51:33 -0400

    made static public

--- modules/module_28/Follow Along/express_crash_course/index.js
+++ modules/module_28/Follow Along/express_crash_course/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+//hard way to access files in public
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// set static folder
+app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
 
