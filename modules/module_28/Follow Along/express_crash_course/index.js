769af2cd9813269bebe2393994fe1357bd43a0bc
JAGeorge54 <jesse.george54@gmail.com>
2024-03-15 14:16:48 -0400

    added members object and all members get request

--- modules/module_28/Follow Along/express_crash_course/index.js
+++ modules/module_28/Follow Along/express_crash_course/index.js
@@ -2,11 +2,38 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Williams',
+        email: 'bob@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Shannon Jackson',
+        email: 'shannon@gmail.com',
+        status: 'active'
+    }
+];
+
+//Gets all Members
+app.get('/api/members', (req, res) => res.json(members));
+
+
+
+
 //hard way to access files in public
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
-
 // set static folder
 app.use(express.static('public'));
 
