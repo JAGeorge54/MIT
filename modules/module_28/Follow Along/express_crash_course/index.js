c1a1794b3dc681b5d321424224bd613e07013ce6
JAGeorge54 <jesse.george54@gmail.com>
2024-03-15 13:37:17 -0400

    added path module

--- modules/module_28/Follow Along/express_crash_course/index.js
+++ modules/module_28/Follow Along/express_crash_course/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('<h1>Hello World!</h1>');
 });
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Running on port: ${port}!`);
