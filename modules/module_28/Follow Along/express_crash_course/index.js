cf43b3caf297cfa35cd2018433d054c18390fdd0
JAGeorge54 <jesse.george54@gmail.com>
2024-03-15 15:45:04 -0400

    moved member api to routes folder

--- modules/module_28/Follow Along/express_crash_course/index.js
+++ modules/module_28/Follow Along/express_crash_course/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const members = require('./Members');
 const logger = require('./middleware/logger');
 
 const app = express();
@@ -8,20 +7,7 @@ const app = express();
 //Init middleware
 // app.use(logger);
 
-//Gets all Members
-app.get('/api/members', (req, res) => res.json(members));
 
-//Gets single Member
-app.get('/api/members/:id', (req, res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id));
-
-    if (found) {
-        res.json(members.filter(member => member.id === parseInt(req.params.id)));
-    } else {
-        res.status(400).json({ msg: `No member with the id of ${req.params.id}`});
-    }
-        
-})
 
 //hard way to access files in public
 // app.get('/', (req, res) => {
@@ -29,6 +15,9 @@ app.get('/api/members/:id', (req, res) => {
 // });
 // set static folder
 app.use(express.static('public'));
+
+//Members API Routes
+app.use('/api/members', require('./routes/api/members'));
 
 const port = process.env.PORT || 3000;
 
