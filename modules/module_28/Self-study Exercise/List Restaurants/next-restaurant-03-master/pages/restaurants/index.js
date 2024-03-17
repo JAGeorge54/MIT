65eb897f77753572604379f8a96d38bfcfdf8831
JAGeorge54 <jesse.george54@gmail.com>
2024-03-17 13:51:09 -0400

    hard wired restaurant array

--- modules/module_28/Self-study Exercise/List Restaurants/next-restaurant-03-master/pages/restaurants/index.js
+++ modules/module_28/Self-study Exercise/List Restaurants/next-restaurant-03-master/pages/restaurants/index.js
@@ -3,6 +3,11 @@ import styles from '../../styles/Home.module.css'
 import Link from 'next/Link'
 
 export default function Home() {
+  const restaurants = [
+    {name: 'WoodsHill'},
+    {name:'Fiorellas'},
+    {name: 'Karma'}
+  ]
   return (
     <h1>Restaurant Top Page!</h1>
     )
