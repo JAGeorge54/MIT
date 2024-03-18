456d0eb5b1303a4a71be3a422e45fb9edb01499d
JAGeorge54 <jesse.george54@gmail.com>
2024-03-18 10:02:09 -0400

    added contact page

---
+++ modules/module_28/Self-study Exercise/List Restaurants/next-restaurant-03-master/pages/contact.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Contact Page</h1>
+        <h2>Jesse George</h2>
+        <h2>jesse.george54@gmail.com</h2>
+    </div>
+    )
+}
