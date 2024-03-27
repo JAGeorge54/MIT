a235111cd7a84685189af307b6f8f72d7b675ea1
JAGeorge54 <jesse.george54@gmail.com>
2024-03-27 15:32:40 -0400

    fixed .env database variable

--- modules/module_21/Learning/express-locallibrary-tutorial/app.js
+++ modules/module_21/Learning/express-locallibrary-tutorial/app.js
@@ -15,7 +15,7 @@ var app = express();
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "process.env.MONGO_URI";
+const mongoDB = process.env.MONGO_URI;
 
 main().catch((err) => console.log(err));
 async function main() {
