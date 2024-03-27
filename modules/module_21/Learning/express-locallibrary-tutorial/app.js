cf49faa42803c39fd3baa6ce6a0f5cee9279b720
JAGeorge54 <jesse.george54@gmail.com>
2024-03-27 15:13:17 -0400

    required catalog

--- modules/module_21/Learning/express-locallibrary-tutorial/app.js
+++ modules/module_21/Learning/express-locallibrary-tutorial/app.js
@@ -5,8 +5,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+
 
 var app = express();
 
