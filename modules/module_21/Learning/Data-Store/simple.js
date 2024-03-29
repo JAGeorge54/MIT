75cd30ea405b8fa0a9da9b8ef50421a081d850d3
JAGeorge54 <jesse.george54@gmail.com>
2024-03-29 10:38:19 -0400

    added files

--- modules/module_21/Learning/Data-Store/simple.js
+++ modules/module_21/Learning/Data-Store/simple.js
@@ -7,25 +7,42 @@ var db      = low(adapter);
 // ---------------------------
 db.defaults({ posts: []}).write();
  
-// console.log(db.get('posts').value());
+console.log(db.get('posts').value());
 
 // Add a post
-db.get('posts')
-  .push({ id: 1, title: 'lowdb is awesome', published: true})
-  .write()
+// db.get('posts')
+//   .push({ id: 2, title: 'lowdb is awesome', published: true})
+//   .write()
+// db.get('posts')
+//   .push({ id: 3, title: 'lowdb is awesome', published: true})
+//   .write()
+// db.get('posts')
+//   .push({ id: 4, title: 'lowdb is awesome', published: true})
+//   .write()
 
 // count posts
 // ----------------------------
 // YOUR CODE
+let count = db.get("posts").size().value();
+console.log("count", count);
 
 // find all posts ids
 // ----------------------------
 // YOUR CODE
+let allPosts = db
+  .get("posts")
+  .value()
+  .map((post) => post.id);
+console.log("allPosts", allPosts);
 
 // all matches of published false
 // ----------------------------
 // YOUR CODE
+let unpublished = db.get("posts").filter({ published: false }).value();
+console.log("unpublished", unpublished);
 
 // find post with published false
 // ----------------------------
 // YOUR CODE
+let unpublishedPost = db.get("posts").find({ published: false }).value();
+console.log("unpublishedPost", unpublishedPost);
